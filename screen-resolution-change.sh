045d07d33fd662018b0e7ffd79f91607a9d695d4
tsotnep <tsotnep@gmail.com>
2016-02-26 15:14:50 +0200

    selects monitor and changes resolution, but pixel xy should be corrected

--- screen-resolution-change.sh
+++ screen-resolution-change.sh
@@ -16,25 +16,56 @@
 # receive SCALE variable from user, you can change "1.2" into other default value
 SCALE=${1:-"1.2"}
 
+# get the name of connected screen
+connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+activeOutput=($(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/"))
+echo ${activeOutput[*]}
+read -p "Type number of screen from the list above, you want to change 0,1,2.. : " resp
+scrN=${activeOutput[$resp]}
+# scrR=
+# echo $scrN
 # get current Resolution
-R=$(xrandr | grep "\*")
+Resolutions=($(xrandr | grep "\*"))
+
+# echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+Res=()
+for i in "${Resolutions[@]}"
+do
+    if [[ "$i" == *"x"* ]] ; then
+        # echo "FOUNDDDDDDDDDD"
+        # echo $i
+        Res+=($i)
+    fi
+done
+# echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+# echo ${Res[*]}
+
+R=${Res[$resp]}
+# echo "R is " $R
 
 # get X and Y
 R=$(echo $R | tr x ' ')
+# echo "R is " $R
 X=$(echo $R | cut -d ' ' -f1)
+# echo "X is " $X
 Y=$(echo $R | cut -d ' ' -f2)
+# echo "Y is " $Y
 
 # get values for --panning
 sXfloat=$(echo "$X * $SCALE" | bc)
 sYfloat=$(echo "$Y * $SCALE" | bc)
 sX=${sXfloat%.*}
 sY=${sYfloat%.*}
+# echo "sX is " $sX
+# echo "sY is " $sY
 
-# get the name of connected screen
-connectedScreen=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+# TODO: when several monitors are conected, pixel starting and ending points needs to be fixed
+  # when u type xrandr, it prints ...1639x921+0+0... here, 0+0 is xy of pixel starting points, so when there are several screens,
+  # they should be modified correctly, otherwise it fuckes up
 
 # set the resolution and panning
-xrandr --output $connectedScreen --scale "$SCALE"x"$SCALE" --panning "$sX"x"$sY"
+xrandr --output $scrN --scale "$SCALE"x"$SCALE" --panning "$sX"x"$sY"
 
 #other commands :
 #$ xdpyinfo  | grep dimensions    # to see the actual values of current resolution
